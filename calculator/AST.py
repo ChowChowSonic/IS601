7d48ba24e2d8861e3d0597feddadc99f9ad7a2a7
ChowChowSonic <joeyant1367@gmail.com>
2025-02-06 14:50:01 -0500

    added NumberExprAST & AdditionStmtAST classes

--- calculator/AST.py
+++ calculator/AST.py
@@ -8,3 +8,24 @@ class AST:
         raise NotImplementedError(
             "AST is an abstract class, AST.codegen() was not implemented!"
         )
+
+
+class NumberExprAST(AST):
+    """A class representing a number; 1234.5"""
+
+    def __init__(self, HS):
+        self.HS = HS
+
+    def codegen(self):
+        return self.HS
+
+
+class AdditionStmtAST(AST):
+    """A class representing an addition statement; a+b"""
+
+    def __init__(self, LHS, RHS):
+        self.LHS = LHS
+        self.RHS = RHS
+
+    def codegen(self):
+        return self.LHS.codegen() + self.RHS.codegen()
