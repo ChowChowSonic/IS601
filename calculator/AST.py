704e6b2a662dd77bdf38b18a9d36a27c3cbaf0a5
ChowChowSonic <joeyant1367@gmail.com>
2025-02-08 23:09:45 -0500

    removed code uplication from AST class

--- calculator/AST.py
+++ calculator/AST.py
@@ -4,10 +4,23 @@
 class AST:
     """The base class class for an Abstract Syntax tree"""
 
+    def __init__(self):
+        raise NotImplementedError("AST is an abstract class, AST() is not callable!")
+
     def codegen():
         raise NotImplementedError(
             "AST is an abstract class, AST.codegen() was not implemented!"
         )
+
+
+class BinaryAST(AST):
+
+    def __init__(self, LHS: AST, RHS: AST):
+        self.LHS = LHS
+        self.RHS = RHS
+
+    def codegen(self):
+        raise NotImplementedError("BinaryAST does not implement a codegen function!")
 
 
 class NumberExprAST(AST):
@@ -20,34 +33,22 @@ class NumberExprAST(AST):
         return self.HS
 
 
-class AdditionStmtAST(AST):
+class AdditionStmtAST(BinaryAST):
     """A class representing an addition statement; a+b"""
-
-    def __init__(self, LHS:AST, RHS:AST):
-        self.LHS = LHS
-        self.RHS = RHS
 
     def codegen(self):
         return self.LHS.codegen() + self.RHS.codegen()
 
 
-class SubtractionStmtAST(AST):
+class SubtractionStmtAST(BinaryAST):
     """A class representing a subtraction statement; a-b"""
-
-    def __init__(self, LHS:AST, RHS:AST):
-        self.LHS = LHS
-        self.RHS = RHS
 
     def codegen(self):
         return self.LHS.codegen() - self.RHS.codegen()
 
 
-class DivisionStmtAST(AST):
+class DivisionStmtAST(BinaryAST):
     """A class representing a division statement; a/b"""
-
-    def __init__(self, LHS:AST, RHS:AST):
-        self.LHS = LHS
-        self.RHS = RHS
 
     def codegen(self):
         r = self.RHS.codegen()
@@ -56,12 +57,8 @@ class DivisionStmtAST(AST):
         return self.LHS.codegen() / r
 
 
-class MultiplicationStmtAST(AST):
+class MultiplicationStmtAST(BinaryAST):
     """A class representing a multiplication statement; a*b"""
-
-    def __init__(self, LHS:AST, RHS:AST):
-        self.LHS = LHS
-        self.RHS = RHS
 
     def codegen(self):
         return self.LHS.codegen() * self.RHS.codegen()
