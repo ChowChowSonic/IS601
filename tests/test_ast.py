6608cbbf54392c5c286d9fb2f111079f4361bf7d
ChowChowSonic <joeyant1367@gmail.com>
2025-02-10 15:32:34 -0500

    import corrections, added test_calculation() and test_history()

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,53 +1,48 @@
 """My Calculator Test"""
 
 import pytest
-from calculator import AST
+from calculator.AST import (
+    NumberExprAST,
+    AdditionStmtAST,
+    SubtractionStmtAST,
+    MultiplicationStmtAST,
+    DivisionStmtAST,
+    AST,
+    BinaryAST,
+)
 
 
 def test_ast():
     """Testing Abstract Syntax Tree"""
-    # AST.AST() & AST.AST.codegen() throws an error when called
+    # AST() & AST.codegen() throws an error when called
     with pytest.raises(NotImplementedError):
-        AST.AST()
+        AST()
 
 
 def test_binaryast():
     """Test that the Binary AST class throws an error"""
     with pytest.raises(NotImplementedError):
-        bast = AST.BinaryAST(AST.NumberExprAST(0), AST.NumberExprAST(0))
+        bast = BinaryAST(NumberExprAST(0), NumberExprAST(0))
         bast.codegen()
 
 
 def test_addition():
     """Test that the addition function works"""
-    assert (
-        AST.AdditionStmtAST(AST.NumberExprAST(2), AST.NumberExprAST(2)).codegen() == 4
-    )
+    assert AdditionStmtAST(NumberExprAST(2), NumberExprAST(2)).codegen() == 4
 
 
 def test_subtraction():
     """Test that the subtraction function works"""
-    assert (
-        AST.SubtractionStmtAST(AST.NumberExprAST(2), AST.NumberExprAST(2)).codegen()
-        == 0
-    )
+    assert SubtractionStmtAST(NumberExprAST(2), NumberExprAST(2)).codegen() == 0
 
 
 def test_multiplication():
     """Test that the multiplication function works"""
-    assert (
-        AST.MultiplicationStmtAST(AST.NumberExprAST(3), AST.NumberExprAST(2)).codegen()
-        == 6
-    )
+    assert MultiplicationStmtAST(NumberExprAST(3), NumberExprAST(2)).codegen() == 6
 
 
 def test_division():
     """Test that the division function works"""
-    assert (
-        AST.DivisionStmtAST(AST.NumberExprAST(2), AST.NumberExprAST(2)).codegen() == 1
-    )
+    assert DivisionStmtAST(NumberExprAST(2), NumberExprAST(2)).codegen() == 1
     with pytest.raises(ZeroDivisionError):
-        assert (
-            AST.DivisionStmtAST(AST.NumberExprAST(2), AST.NumberExprAST(0)).codegen()
-            == 1
-        )
+        assert DivisionStmtAST(NumberExprAST(2), NumberExprAST(0)).codegen() == 1
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,47 @@
 """Tests the arg parsing capabilities of the calculator module."""
-from calculator import parse_args
+
+from calculator import parse_args, lower_operator_precidence, higher_operator_precidence
+from calculator.AST import (
+    AdditionStmtAST,
+    DivisionStmtAST,
+    SubtractionStmtAST,
+    MultiplicationStmtAST,
+    NumberExprAST,
+)
+from calculator.calculations import Calculations
+
 
 def test_argparse():
     """Tests the arg parsing capabilities of the calculator module."""
     assert parse_args("1+2+3", "+4") == [1, "+", 2, "+", 3, "+", 4]
     assert parse_args("5-6*7/8") == [5, "-", 6, "*", 7, "/", 8]
     assert parse_args("9^10") == [9, "^", 10]
+
+
+def test_calculation():
+    """Tests the calculation function using long strings of input"""
+    assert lower_operator_precidence(parse_args("2+3*4")).codegen() == 14
+    assert lower_operator_precidence(parse_args("5-6*7/8")).codegen() == -0.25
+    assert lower_operator_precidence(parse_args("9+10")).codegen() == 19
+    assert higher_operator_precidence(parse_args("9*10")).codegen() == 90
+
+
+def test_history():
+    """Tests the history functionality of the Calculations class"""
+    Calculations.add_to_history(AdditionStmtAST(NumberExprAST(5), NumberExprAST(5)))
+    Calculations.add_to_history(AdditionStmtAST(NumberExprAST(10), NumberExprAST(20)))
+    Calculations.add_to_history(
+        SubtractionStmtAST(NumberExprAST(10), NumberExprAST(20))
+    )
+    Calculations.add_to_history(
+        MultiplicationStmtAST(NumberExprAST(10), NumberExprAST(2))
+    )
+    Calculations.add_to_history(DivisionStmtAST(NumberExprAST(10), NumberExprAST(20)))
+    assert Calculations.get_from_history(0).codegen() == 10
+    assert Calculations.get_from_history(1).codegen() == 30
+    assert Calculations.get_from_history(2).codegen() == -10
+    assert Calculations.get_from_history(3).codegen() == 20
+    assert Calculations.get_from_history(4).codegen() == 0.5
+    assert len(Calculations.get_history()) == 5
+    Calculations.clear_history()
+    assert len(Calculations.get_history()) == 0
