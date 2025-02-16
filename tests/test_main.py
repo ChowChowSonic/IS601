48cf8525ed1c2f8cfed5699ebdde4c4ea7f9192b
ChowChowSonic <joeyant1367@gmail.com>
2025-02-16 17:26:25 -0500

    added test_from_pipe() to test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,8 @@
+"""Testing the main driver code """
+
 import pytest
-from calculator.AST import (
-    AdditionStmtAST,
-    DivisionStmtAST,
-    SubtractionStmtAST,
-    MultiplicationStmtAST,
-    NumberExprAST,
-    AST,
-)
 from main import calculate_and_print
+import sys
 
 
 @pytest.mark.parametrize(
@@ -29,3 +24,10 @@ def test_calculator(test_input, expected):
     assert calculate_and_print(test_input) == expected
     with pytest.raises(ZeroDivisionError):
         assert calculate_and_print("1/0") == 1
+
+
+def test_from_pipe():
+    oldstdin = sys.stdin
+    sys.stdin = open("testdata.txt")
+    assert calculate_and_print([]) is None
+    sys.stdin = oldstdin
