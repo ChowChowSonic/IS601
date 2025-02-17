7f513e7d25b5ea194277fbee8b3c17f96a132578
ChowChowSonic <joeyant1367@gmail.com>
2025-02-17 18:01:06 -0500

    pylint fix for conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
 """ Adds random testing data from faker to the test cases"""
 
 from faker import Faker
-import pytest
-from calculator.AST import AST
 from calculator.AST import NumberExprAST, AST
 
 
@@ -10,8 +8,8 @@ faker = Faker()
 
 
 def generate_test_data(len_data):
-
-    for i in range(len_data):
+    """Generates tests dadta"""
+    for _ in range(len_data):
         a = NumberExprAST(faker.random_number(digits=1))
         b = NumberExprAST(faker.random_number(digits=1))
         op = faker.random_element(elements=["add", "subtract", "multiply", "divide"])
@@ -22,6 +20,7 @@ def generate_test_data(len_data):
 
 
 def pytest_addoption(parser):
+    """Adds an option to pytest"""
     parser.addoption(
         "--num_records",
         action="store",
@@ -32,21 +31,15 @@ def pytest_addoption(parser):
 
 
 def pytest_generate_tests(metafunc):
+    """Generates the test/result pairs"""
     # Check if the test is expecting any of the dynamically generated fixtures
-    if {"a", "b", "operation", "expected"}.intersection(set(metafunc.fixturenames)):
+    if {"a_input", "b_input", "operation", "expected"}.intersection(set(metafunc.fixturenames)):
         num_records = metafunc.config.getoption("num_records")
-        # Adjust the parameterization to include both operation_name and operation for broad compatibility
-        # Ensure 'operation_name' is used for identifying the operation in Calculator class tests
+        # Adjust the parameterization to include both operation_name and
+        # operation for broad compatibility. Ensure 'operation_name'
+        # is used for identifying the operation in Calculator class tests
         # 'operation' (function reference) is used for Calculation class tests.
         parameters = list(generate_test_data(num_records))
         # Modify parameters to fit test functions' expectations
-        modified_parameters = [
-            (
-                a,
-                b,
-                op_name,
-                expected,
-            )
-            for a, b, op_name, expected in parameters
-        ]
-        metafunc.parametrize("a,b,operation,expected", modified_parameters)
+        modified_parameters = list(parameters)
+        metafunc.parametrize("a_input,b_input,operation,expected", modified_parameters)
