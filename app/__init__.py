47c27e3538c85739670031b88a441034a5020775
ChowChowSonic <joeyant1367@gmail.com>
2025-02-24 17:46:25 -0500

    added execute command in 'app' class

--- app/__init__.py
+++ app/__init__.py
@@ -1,38 +1,35 @@
-from commands import CommandHandler
+from app.commands import CommandHandler
 import os, sys, importlib.util
 
 
 class App:
-    def __init__(self):
-        self.handler = CommandHandler()
+	def __init__(self):
+		self.handler = CommandHandler()
 
-    def _import_plugins(self, plugins_dir="plugins"):
-        plugins_path = os.path.abspath(plugins_dir)
-        modules = {}
-        sys.path.insert(0, os.path.abspath(os.getcwd()))
-        for entry in os.listdir(plugins_path):
-            entry_path = os.path.join(plugins_path, entry)
-
-            # Check if it's a valid Python package (subdirectory with __init__.py)
-            if os.path.isdir(entry_path) and os.path.isfile(
+	def _import_plugins(self, plugins_dir:str="plugins"):
+		plugins_path = os.path.abspath(plugins_dir)
+		modules = {}
+		sys.path.insert(0, os.path.abspath(os.getcwd()))
+		for entry in os.listdir(plugins_path):
+			entry_path = os.path.join(plugins_path, entry)
+			if os.path.isdir(entry_path) and os.path.isfile(
                 os.path.join(entry_path, "__init__.py")
             ):
-                module_name = entry  # Use the subdirectory name as the module name
-                init_py = os.path.join(entry_path, "__init__.py")
+				module_name = entry  # Use the subdirectory name as the module name
+				init_py = os.path.join(entry_path, "__init__.py")
+				spec = importlib.util.spec_from_file_location(
+					module_name, init_py, submodule_search_locations=[entry_path]
+				)
+				if spec and spec.loader:
+					module = importlib.util.module_from_spec(spec)
+					spec.loader.exec_module(module)
+					modules[module_name] = module
+		return modules
+    
+	def start(self):
+		plugins = self._import_plugins()
+		for k in plugins:
+			self.handler.register_command(k, getattr(plugins[k], k)())
 
-                # Load the module directly from its path
-                spec = importlib.util.spec_from_file_location(
-                    module_name, init_py, submodule_search_locations=[entry_path]
-                )
-                if spec and spec.loader:
-                    module = importlib.util.module_from_spec(spec)
-                    spec.loader.exec_module(module)
-                    modules[module_name] = module
-
-        return modules
-
-    def start(self):
-        plugins = self._import_plugins()
-        for k in plugins:
-            self.handler.register_command(k, getattr(plugins[k], k)())
-            self.handler.execute_command(k)
+	def execute_command(self, cmd: str, args: list[str]): 
+		self.handler.execute_command(cmd, args)
