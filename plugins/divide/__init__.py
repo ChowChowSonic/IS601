f6f4c6fe702e67b52315b5e6975c481febf7febc
ChowChowSonic <joeyant1367@gmail.com>
2025-02-25 13:10:21 -0500

    fix to divide and multiply commands for new project structure

--- plugins/divide/__init__.py
+++ plugins/divide/__init__.py
@@ -3,4 +3,4 @@ class divide(Command):
 	def __init__(self):
 		pass 
 	def execute(self, args:list[str]):
-		print("The result of",args[0],"add",args[1],"is equal to",int(args[0])+int(args[1])) 
+		print("The result of",args[0],"divide",args[1],"is equal to",int(args[0])//int(args[1])) 
--- plugins/multiply/__init__.py
+++ plugins/multiply/__init__.py
@@ -3,4 +3,4 @@ class multiply(Command):
 	def __init__(self):
 		pass
 	def execute(self, args:list[str]):
-		print("The result of",args[0],"add",args[1],"is equal to",int(args[0])+int(args[1])) 
+		print("The result of",args[0],"multiply",args[1],"is equal to",int(args[0])*int(args[1])) 
