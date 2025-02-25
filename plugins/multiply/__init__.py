75a8a0554c34af3ae5563dec62501884a2bc54cc
ChowChowSonic <joeyant1367@gmail.com>
2025-02-25 14:45:14 -0500

    added multiprocessing for multiplication

--- plugins/multiply/__init__.py
+++ plugins/multiply/__init__.py
@@ -1,6 +1,9 @@
 from app.commands import Command
+from multiprocessing import Process
 class multiply(Command):
 	def __init__(self):
 		pass
 	def execute(self, args:list[str]):
-		print("The result of",args[0],"multiply",args[1],"is equal to",int(args[0])*int(args[1])) 
+		p=Process(target=print, args=("The result of",args[0],"multiply",args[1],"is equal to",int(args[0])*int(args[1]))) 
+		p.start()
+		p.join()
