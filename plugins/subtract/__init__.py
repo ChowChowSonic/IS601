1e608f8135b3b5aa53709144fdd8b5ad2dc84e7c
ChowChowSonic <joeyant1367@gmail.com>
2025-03-03 13:26:22 -0500

    updated requirements.txt

--- plugins/subtract/__init__.py
+++ plugins/subtract/__init__.py
@@ -4,6 +4,6 @@ class subtract(Command):
 	def __init__(self):
 		pass 
 	def execute(self, args:list[str]):
-		p=Process(target=print,args=("The result of",args[0],"subtract",args[1],"is equal to",int(args[0])-int(args[1])))
-		p.start()
-		p.join()
+		print("The result of",args[0],"subtract",args[1],"is equal to",int(args[0])-int(args[1]))
+		# p.start()
+		# p.join()
